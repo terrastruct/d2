fc98a0560c11ae54415162a70d4a34a8087f7b0b
Júlio César Batista <julio@terrastruct.com>
2022-11-04 17:04:26 -0300

    Fix lint

--- e2etests/report/main.go
+++ e2etests/report/main.go
@@ -143,7 +143,7 @@ func main() {
 		path := os.Getenv("REPORT_OUTPUT")
 		f, err := os.Create(path)
 		if err != nil {
-			panic(fmt.Errorf("Error creating file `%s`. %v", path, err))
+			panic(fmt.Errorf("error creating file `%s`. %v", path, err))
 		}
 		if err := tmpl.Execute(f, tmplData); err != nil {
 			panic(err)
