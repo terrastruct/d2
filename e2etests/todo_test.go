e8fbf64569923ce9b64008da084417053ec89ab3
Gavin Nishizawa <gavin@terrastruct.com>
2022-11-07 11:14:48 -0800

    add md code block tests

--- e2etests/todo_test.go
+++ e2etests/todo_test.go
@@ -10,10 +10,36 @@ func testTodo(t *testing.T) {
 		// https://github.com/terrastruct/d2/issues/24
 		// string monstrosity from not being able to escape backticks within string literals
 		{
-			skip: true,
-			name: "backtick",
+			skip: false,
+			name: "md_code_inline",
 			script: `md: |md
   ` + "`" + "code`" + `
+|
+a -> md -> b
+`,
+		},
+		{
+			skip: false,
+			name: "md_code_block_fenced",
+			script: `md: |md
+` + "```" + `
+{
+	fenced: "block",
+	of: "json",
+}
+` + "```" + `
+|
+a -> md -> b
+`,
+		},
+		{
+			skip: false,
+			name: "md_code_block_indented",
+			script: `md: |md
+    {
+        indented: "block",
+        of: "json",
+    }
 |
 a -> md -> b
 `,
