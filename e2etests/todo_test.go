31d0124d311d06e1c1dd3e2d7f3799b4fca2544e
Gavin Nishizawa <gavin@terrastruct.com>
2022-11-11 15:41:24 -0800

    add todo for dagre layout

--- e2etests/todo_test.go
+++ e2etests/todo_test.go
@@ -6,7 +6,16 @@ import (
 )
 
 func testTodo(t *testing.T) {
-	tcs := []testCase{}
+	tcs := []testCase{
+		{
+			// issue https://github.com/terrastruct/d2/issues/71
+			name: "container_child_edge",
+			script: `
+container.first -> container.second: 1->2
+container -> container.second: c->2
+`,
+		},
+	}
 
 	runa(t, tcs)
 }
