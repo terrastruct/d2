756a2315d96922f0fc512ef45370933a88495f9d
Anmol Sethi <anmol@terrastruct.com>
2022-11-16 03:16:57 -0800

    ci: Fixes

--- ci/release/template/scripts/uninstall.sh
+++ ci/release/template/scripts/uninstall.sh
@@ -4,7 +4,7 @@ cd -- "$(dirname "$0")/.."
 . ./scripts/lib.sh
 
 main() {
-  if [ ! -e "${PREFIX-}" ]; then
+  if [ -z "${PREFIX-}" ]; then
     echoerr "\$PREFIX must be set to a unix prefix directory from which to uninstall d2 like /usr/local"
     return 1
   fi
