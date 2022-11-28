9d426c4d4619f97d25fe5e591495f11578cb71c2
Alexander Wang <alex@terrastruct.com>
2022-11-27 20:06:17 -0800

    2022-11-27 08:06:17PM

--- d2renderers/d2latex/setup.js
+++ d2renderers/d2latex/setup.js
@@ -2,5 +2,5 @@ const adaptor = MathJax._.adaptors.liteAdaptor.liteAdaptor();
 MathJax._.handlers.html_ts.RegisterHTMLHandler(adaptor)
 const html = MathJax._.mathjax.mathjax.document('', {
   InputJax: new MathJax._.input.tex_ts.TeX(),
-  OutputJax: new MathJax._.output.svg_ts.SVG({ fontCache: "none" }),
+  OutputJax: new MathJax._.output.svg_ts.SVG(),
 });
