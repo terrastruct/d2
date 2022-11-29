ea876e051b3f2669770d6ac8d19ec3641686a179
Anmol Sethi <anmol@terrastruct.com>
2022-11-29 14:18:52 -0800

    install.sh: Improve dry run message and add short flag -d

--- ci/release/template/scripts/lib.sh
+++ ci/release/template/scripts/lib.sh
@@ -62,7 +62,7 @@ should_color() {
       _COLOR=1
       return 0
     else
-      printf '$COLOR must be 0, 1, false or true but got %s' "$COLOR" >&2
+      printf '$COLOR must be 0, 1, false or true but got %s\n' "$COLOR" >&2
     fi
   fi
 
@@ -219,8 +219,9 @@ header() {
 }
 
 bigheader() {
+  set -- "$(echo "$*" | sed "s/^/ * /")"
   logp "/**
- * $1
+$*
  **/"
 }
 
