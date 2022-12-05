5deb9b2d7ddd08cb09a8b4ef14f371249b2f9351
Alexander Wang <alex@terrastruct.com>
2022-12-05 11:22:16 -0800

    blend groups

--- d2exporter/export.go
+++ d2exporter/export.go
@@ -99,6 +99,10 @@ func toShape(obj *d2graph.Object, theme *d2themes.Theme) d2target.Shape {
 	shape.Italic = text.IsItalic
 	shape.FontSize = text.FontSize
 
+	if obj.IsSequenceDiagramGroup() {
+		shape.Blend = true
+	}
+
 	applyStyles(shape, obj)
 	applyTheme(shape, obj, theme)
 	shape.Color = text.GetColor(theme, shape.Italic)
