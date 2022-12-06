563b45dd0128d3a103fdce267261f79c3c7e719a
Anmol Sethi <anmol@terrastruct.com>
2022-12-06 08:53:50 -0800

    release/install.sh: Install executable with .exe suffix on Windows
    Otherwise Go doesn't let you execute it lol.

--- ci/release/template/scripts/install.sh
+++ ci/release/template/scripts/install.sh
@@ -6,7 +6,14 @@ cd -- "$(dirname "$0")/.."
 main() {
   ensure_prefix_sh_c
   "$sh_c" mkdir -p "$PREFIX/bin"
-  "$sh_c" install ./bin/d2 "$PREFIX/bin/d2"
+
+  ensure_os
+  if [ "$OS" = windows ]; then
+    "$sh_c" install ./bin/d2 "$PREFIX/bin/d2.exe"
+  else
+    "$sh_c" install ./bin/d2 "$PREFIX/bin/d2"
+  fi
+
   "$sh_c" mkdir -p "$PREFIX/share/man/man1"
   "$sh_c" install ./man/d2.1 "$PREFIX/share/man/man1"
 }
