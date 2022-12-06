b5d863ae029e0c51a3510dc22372cca36b6be264
Anmol Sethi <anmol@terrastruct.com>
2022-12-06 05:17:17 -0800

    ci: Update

--- ci/release/template/scripts/lib.sh
+++ ci/release/template/scripts/lib.sh
@@ -65,7 +65,7 @@ temp_exittrap() {
 temppath() {
   ensure_tmpdir
   while true; do
-    temppath=$_TMPDIR/$(</dev/urandom head -c8 | base32)
+    temppath=$_TMPDIR/$(</dev/urandom od -N8 -tx -An -v | tr -d '[:space:]')
     if [ ! -e "$temppath" ]; then
       echo "$temppath"
       return
