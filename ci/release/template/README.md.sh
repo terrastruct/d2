ce0e2b332638a1cc1a1e7789a68d78be4efb9c02
Anmol Sethi <anmol@terrastruct.com>
2022-12-07 19:14:05 -0800

    release: On windows use .exe and alter README
    Closes #388

--- ci/release/template/README.md.sh
+++ ci/release/template/README.md.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -eu
+cd -- "$(dirname "$0")/../../.."
+. ./ci/sub/lib.sh
 
 cat <<EOF
 # d2
@@ -7,6 +9,31 @@ cat <<EOF
 For docs, more installation options and the source code see https://oss.terrastruct.com/d2
 
 version: $VERSION
+os: $OS
+arch: $ARCH
+
+Built with $(go version | grep -o 'go[^ ]\+').
+EOF
+
+ensure_os
+if [ "$OS" = windows ]; then
+  cat <<EOF
+
+We currently do not have a .msi for proper installation on Windows so this release is
+structured the same as our Unix releases. You can still install on Windows still though
+with [MSYS2](https://www.msys2.org/) which emulates a Linux shell for Windows. It also
+enables d2 to show colors in its output.
+
+But if you must install on Windows without MSYS2, for now you'll have to add the d2 binary
+in \`./bin/d2.exe\` to your \`\$PATH\` manually. You can also add the \`./bin\` directory
+to your \`\$PATH\`.
+
+See https://www.wikihow.com/Change-the-PATH-Environment-Variable-on-Windows
+See https://github.com/terrastruct/d2/blob/master/docs/INSTALL.md#windows
+EOF
+fi
+
+cat <<EOF
 
 ## Install
 
