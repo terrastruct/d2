72dfbf708f328f6c236176daba78d536fde5a425
Anmol Sethi <anmol@terrastruct.com>
2022-12-18 12:41:22 -0800

    ci/release/build.sh: Automatically clone ci submodule

--- ci/release/build.sh
+++ ci/release/build.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 set -eu
+if [ ! -e "$(dirname "$0")/../../ci/sub/.git" ]; then
+  set -x
+  git submodule update --init
+  set +x
+fi
+. "$(dirname "$0")/../../ci/sub/lib.sh"
 cd -- "$(dirname "$0")/../.."
-. ./ci/sub/lib.sh
 
 help() {
   cat <<EOF
