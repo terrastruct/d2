60b6133b2cf2910fb6700409f7d9929b70175b1e
Alexander Wang <alex@terrastruct.com>
2022-12-28 15:16:18 -0800

    remove commented

--- d2renderers/d2svg/appendix/appendix.go
+++ d2renderers/d2svg/appendix/appendix.go
@@ -169,9 +169,6 @@ func generateTooltipLine(i, y int, text string, ruler *textmeasure.Ruler) (strin
 
 	dims := d2graph.GetTextDimensions(nil, ruler, mtext, nil)
 
-	// filter: drop-shadow(0px 0px 32px #4444dd);
-	// filter: drop-shadow(0px 0px 32px rgba(31, 36, 58, 0.1));
-	// TODO box-shadow: 0px 0px 32px rgba(31 36 58 0.1);
 	line := fmt.Sprintf(`<g transform="translate(%d %d)" class="tooltip-icon">%s</g>`,
 		0, y, generateNumberedIcon(i, 0, 0))
 
