070b50a6e22de5c0460622794b8ccedffcef7be2
Bernard Xie <bernard@terrastruct.com>
2023-01-03 14:44:36 -0800

    add check to only run script on root svg

--- d2renderers/d2svg/fitToScreen.js
+++ d2renderers/d2svg/fitToScreen.js
@@ -1,5 +1,10 @@
 window.addEventListener("DOMContentLoaded", () => {
   const svgEl = document.querySelector("svg");
+  const svgID = "d2-svg";
+  svgEl.setAttribute("id", svgID);
+  if (document.documentElement.getAttribute("id") !== svgID) {
+    return;
+  }
   let width = parseInt(svgEl.getAttribute("width"), 10);
   let height = parseInt(svgEl.getAttribute("height"), 10);
   let ratio;
@@ -10,9 +15,7 @@ window.addEventListener("DOMContentLoaded", () => {
   } else if (height > window.innerHeight) {
     ratio = window.innerHeight / height;
   }
-  // Scale svg fit to zoom
   if (ratio) {
-    // body padding is 8px
     svgEl.setAttribute("width", width * ratio - 16);
     svgEl.setAttribute("height", height * ratio - 16);
   }
