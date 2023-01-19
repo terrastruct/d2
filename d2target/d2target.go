d77f7af7c232e14cb3957ad4d05691cfd4483251
Alexander Wang <alex@terrastruct.com>
2023-01-19 12:57:05 -0800

    test

--- d2target/d2target.go
+++ d2target/d2target.go
@@ -307,16 +307,13 @@ func (c Connection) CSSStyle() string {
 }
 
 func (c *Connection) GetLabelTopLeft() *geo.Point {
-	p := label.Position(c.LabelPosition).GetPointOnRoute(
+	return label.Position(c.LabelPosition).GetPointOnRoute(
 		c.Route,
 		float64(c.StrokeWidth),
 		c.LabelPercentage,
 		float64(c.LabelWidth),
 		float64(c.LabelHeight),
 	)
-	p.Y -= float64(c.LabelHeight) / 2
-
-	return p
 }
 
 func (c Connection) GetZIndex() int {
