c8303eb16d7817ef551a937db5f7a42dfe339c78
Apostolis Anastasiou <anastasiou@inaccess.com>
2023-01-20 01:34:06 +0200

    Add xdg-open and friendly error output when can't report "REPORT_OUTPUT" with open or xdg-open

--- ci/e2ereport.sh
+++ ci/e2ereport.sh
@@ -9,7 +9,9 @@ FORCE_COLOR=1 DEBUG=1 go run ./e2etests/report/main.go "$@";
 
 if [ -z "${NO_OPEN:-}" ]; then
   if [ -s "$REPORT_OUTPUT" ]; then
-    open "$REPORT_OUTPUT"
+    open "$REPORT_OUTPUT" 2>/dev/null || \
+    xdg-open "$REPORT_OUTPUT" 2>/dev/null || \
+    echo "could not open $REPORT_OUTPUT with commands 'open' or 'xdg-open'"
   else
     echo "The report is empty"
   fi
