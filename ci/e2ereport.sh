e8906ef0b0ed5c3c9669442be73532f4e634da3f
Apostolis Anastasiou <anastasiou@inaccess.com>
2023-01-20 02:05:35 +0200

    Add test to see if command exists with POSIX 'command'

--- ci/e2ereport.sh
+++ ci/e2ereport.sh
@@ -9,9 +9,13 @@ FORCE_COLOR=1 DEBUG=1 go run ./e2etests/report/main.go "$@";
 
 if [ -z "${NO_OPEN:-}" ]; then
   if [ -s "$REPORT_OUTPUT" ]; then
-    open "$REPORT_OUTPUT" 2>/dev/null || \
-    xdg-open "$REPORT_OUTPUT" 2>/dev/null || \
-    echo "could not open $REPORT_OUTPUT with commands 'open' or 'xdg-open'"
+    if [ -x "$(command -v open)" ]; then
+      open $REPORT_OUTPUT
+    elif [ -x "$(command -v xdg-open)" ]; then
+      xdg-open $REPORT_OUTPUT
+    else
+      echo "Please open $REPORT_OUTPUT"
+    fi
   else
     echo "The report is empty"
   fi
