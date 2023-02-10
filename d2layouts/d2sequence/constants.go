b02979f7e4d0571950a54059acaf3fa0946cff27
Alexander Wang <alex@terrastruct.com>
2023-02-09 19:18:24 -0800

    revert sequence diagram changes

--- d2layouts/d2sequence/constants.go
+++ d2layouts/d2sequence/constants.go
@@ -1,21 +1,21 @@
 package d2sequence
 
-const HORIZONTAL_PAD = 40.
+// leaves at least 25 units of space on the left/right when computing the space required between actors
+const HORIZONTAL_PAD = 50.
 
-const VERTICAL_PAD = 20.
+// leaves at least 25 units of space on the top/bottom when computing the space required between messages
+const VERTICAL_PAD = 50.
 
-const MIN_ACTOR_DISTANCE = 150.
+const MIN_ACTOR_DISTANCE = 250.
 
-const MIN_ACTOR_WIDTH = 100.
+const MIN_ACTOR_WIDTH = 150.
 
-const SELF_MESSAGE_HORIZONTAL_TRAVEL = 74
+const SELF_MESSAGE_HORIZONTAL_TRAVEL = 100.
 
 const GROUP_CONTAINER_PADDING = 24.
 
-const GROUP_LABEL_PADDING = 30
-
 // min vertical distance between messages
-const MIN_MESSAGE_DISTANCE = 40.
+const MIN_MESSAGE_DISTANCE = 80.
 
 // default size
 const SPAN_BASE_WIDTH = 12.
@@ -24,13 +24,13 @@ const SPAN_BASE_WIDTH = 12.
 const SPAN_DEPTH_GROWTH_FACTOR = 8.
 
 // when a span has a single messages
-const MIN_SPAN_HEIGHT = 60.
+const MIN_SPAN_HEIGHT = 80.
 
 const SPAN_MESSAGE_PAD = 16.
 
 const LIFELINE_STROKE_WIDTH int = 2
 
-const LIFELINE_STROKE_DASH int = 4
+const LIFELINE_STROKE_DASH int = 6
 
 // pad when the actor has the label placed OutsideMiddleBottom so that the lifeline is not so close to the text
 const LIFELINE_LABEL_PAD = 5.
