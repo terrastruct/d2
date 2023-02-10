97e73cf3fd07b430176026eae7d81d36a7aa42ef
Anmol Sethi <hi@nhooyr.io>
2023-02-09 20:09:20 -0800

    Update ci/e2ereport.sh

--- ci/e2ereport.sh
+++ ci/e2ereport.sh
@@ -10,7 +10,7 @@ FORCE_COLOR=1 DEBUG=1 go run ./e2etests/report/main.go "$@";
 if [ -z "${NO_OPEN:-}" ]; then
   if [ -s "$REPORT_OUTPUT" ]; then
     if command -v open >/dev/null; then
-      open $REPORT_OUTPUT
+      open "$REPORT_OUTPUT"
     elif command -v xdg-open >/dev/null; then
       xdg-open $REPORT_OUTPUT
     else
