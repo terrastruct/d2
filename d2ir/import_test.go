9f0c24f5d7362a68400f64266697444173928d05
Anmol Sethi <hi@nhooyr.io>
2023-06-04 23:32:14 -0700

    fmt

--- d2ir/import_test.go
+++ d2ir/import_test.go
@@ -10,50 +10,50 @@ func testCompileImports(t *testing.T) {
 	t.Parallel()
 
 	tca := []testCase{
-        {
-            name: "value",
-            run: func(t testing.TB) {
-                m, err := compileFS(t, "index.d2", map[string]string{
-                    "index.d2": "x: @x.d2",
-                    "x.d2":     `shape: circle
+		{
+			name: "value",
+			run: func(t testing.TB) {
+				m, err := compileFS(t, "index.d2", map[string]string{
+					"index.d2": "x: @x.d2",
+					"x.d2": `shape: circle
 label: meow`,
-                })
-                assert.Success(t, err)
-                assertQuery(t, m, 3, 0, nil, "")
-                assertQuery(t, m, 2, 0, nil, "x")
-                assertQuery(t, m, 0, 0, "circle", "x.shape")
-                assertQuery(t, m, 0, 0, "meow", "x.label")
-            },
-        },
-        {
-            name: "nested",
-            run: func(t testing.TB) {
-                m, err := compileFS(t, "index.d2", map[string]string{
-                    "index.d2": "x: @x.y",
-                    "x.d2":     `y: {
+				})
+				assert.Success(t, err)
+				assertQuery(t, m, 3, 0, nil, "")
+				assertQuery(t, m, 2, 0, nil, "x")
+				assertQuery(t, m, 0, 0, "circle", "x.shape")
+				assertQuery(t, m, 0, 0, "meow", "x.label")
+			},
+		},
+		{
+			name: "nested",
+			run: func(t testing.TB) {
+				m, err := compileFS(t, "index.d2", map[string]string{
+					"index.d2": "x: @x.y",
+					"x.d2": `y: {
 	shape: circle
 	label: meow
 }`,
-                })
-                assert.Success(t, err)
-                assertQuery(t, m, 3, 0, nil, "")
-                assertQuery(t, m, 2, 0, nil, "x")
-                assertQuery(t, m, 0, 0, "circle", "x.shape")
-                assertQuery(t, m, 0, 0, "meow", "x.label")
-            },
-        },
-        {
-            name: "spread",
-            run: func(t testing.TB) {
-                m, err := compileFS(t, "index.d2", map[string]string{
-                    "index.d2": "...@x.d2",
-                    "x.d2":     "x: wowa",
-                })
-                assert.Success(t, err)
-                assertQuery(t, m, 1, 0, nil, "")
-                assertQuery(t, m, 0, 0, "wowa", "x")
-            },
-        },
+				})
+				assert.Success(t, err)
+				assertQuery(t, m, 3, 0, nil, "")
+				assertQuery(t, m, 2, 0, nil, "x")
+				assertQuery(t, m, 0, 0, "circle", "x.shape")
+				assertQuery(t, m, 0, 0, "meow", "x.label")
+			},
+		},
+		{
+			name: "spread",
+			run: func(t testing.TB) {
+				m, err := compileFS(t, "index.d2", map[string]string{
+					"index.d2": "...@x.d2",
+					"x.d2":     "x: wowa",
+				})
+				assert.Success(t, err)
+				assertQuery(t, m, 1, 0, nil, "")
+				assertQuery(t, m, 0, 0, "wowa", "x")
+			},
+		},
 	}
 
 	runa(t, tca)
