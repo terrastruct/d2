d1e372d8962701dbd0da54609da460f041b5ef9d
Gavin Nishizawa <gavin@terrastruct.com>
2023-06-22 18:49:07 -0700

    add icon_positions test

--- e2etests/stable_test.go
+++ e2etests/stable_test.go
@@ -2780,6 +2780,7 @@ scenarios: {
 		loadFromFile(t, "multiple_box_selection"),
 		loadFromFile(t, "outside_bottom_labels"),
 		loadFromFile(t, "label_positions"),
+		loadFromFile(t, "icon_positions"),
 	}
 
 	runa(t, tcs)
