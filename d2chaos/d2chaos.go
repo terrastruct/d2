b2451eb30c7a2d2d9ca135b629bce49a055d718e
Alexander Wang <alex@terrastruct.com>
2024-06-03 13:18:20 -0600

    fix d2chaos

--- d2chaos/d2chaos.go
+++ d2chaos/d2chaos.go
@@ -269,7 +269,7 @@ func (gs *dslGenState) randStr(n int, inKey bool) string {
 func (gs *dslGenState) randShape() string {
 	for {
 		s := shapes[gs.rand.Intn(len(shapes))]
-		if s != d2target.ShapeImage {
+		if s != d2target.ShapeImage && s != d2target.ShapeText {
 			return s
 		}
 	}
