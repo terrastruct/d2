a8bf4383c28dd3a43a46edb42f914ad5710448cf
Alexander Wang <alex@terrastruct.com>
2024-08-02 15:13:29 -0600

    save

--- d2graph/d2graph.go
+++ d2graph/d2graph.go
@@ -582,7 +582,8 @@ func (obj *Object) GetFill() string {
 
 func (obj *Object) GetStroke(dashGapSize interface{}) string {
 	shape := obj.Shape.Value
-	if obj.Language != "" {
+	if strings.EqualFold(shape, d2target.ShapeCode) ||
+		strings.EqualFold(shape, d2target.ShapeText) {
 		return color.N1
 	}
 	if strings.EqualFold(shape, d2target.ShapeClass) ||
@@ -936,7 +937,9 @@ func (obj *Object) GetLabelSize(mtexts []*d2target.MText, ruler *textmeasure.Rul
 
 	var dims *d2target.TextDimensions
 	switch shapeType {
-	case d2target.ShapeText:
+	case d2target.ShapeClass:
+		dims = GetTextDimensions(mtexts, ruler, obj.Text(), go2.Pointer(d2fonts.SourceCodePro))
+	default:
 		if obj.Language == "latex" {
 			width, height, err := d2latex.Measure(obj.Text().Text)
 			if err != nil {
@@ -952,12 +955,6 @@ func (obj *Object) GetLabelSize(mtexts []*d2target.MText, ruler *textmeasure.Rul
 		} else {
 			dims = GetTextDimensions(mtexts, ruler, obj.Text(), fontFamily)
 		}
-
-	case d2target.ShapeClass:
-		dims = GetTextDimensions(mtexts, ruler, obj.Text(), go2.Pointer(d2fonts.SourceCodePro))
-
-	default:
-		dims = GetTextDimensions(mtexts, ruler, obj.Text(), fontFamily)
 	}
 
 	if shapeType == d2target.ShapeSQLTable && obj.Label.Value == "" {
