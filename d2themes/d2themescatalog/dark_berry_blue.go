30aae50c646cf9559be6b6db980a24d552c26e37
ppenguin <hieronymusv@gmail.com>
2024-08-23 22:55:56 +0200

    Add new darktheme: Dark berry blue
    tweak darkberry theme
    
    t

---
+++ d2themes/d2themescatalog/dark_berry_blue.go
@@ -0,0 +1,25 @@
+package d2themescatalog
+
+import "oss.terrastruct.com/d2/d2themes"
+
+var DarkBerryBlue = d2themes.Theme{
+	ID:   205,
+	Name: "Dark berry blue",
+	Colors: d2themes.ColorPalette{
+		Neutrals: d2themes.DarkNeutral,
+
+		B1: "#E5F3FF",
+		B2: "#BCDDFB",
+		B3: "#77AFE3",
+		B4: "#3363DD",
+		B5: "#1F46B7",
+		B6: "#203586",
+
+		AA2: "#DACEFB",
+		AA4: "#A169D3",
+		AA5: "#6649B5",
+
+		AB4: "#9F7ED1",
+		AB5: "#BA69A6",
+	},
+}
