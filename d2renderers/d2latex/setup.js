cf10aa1da3a5546c19067fcb347f81590cd4fc80
Alexander Wang <alex@terrastruct.com>
2025-01-27 14:43:13 -0700

    latex: do not cache font chars

--- d2renderers/d2latex/setup.js
+++ d2renderers/d2latex/setup.js
@@ -2,7 +2,7 @@ const adaptor = MathJax._.adaptors.liteAdaptor.liteAdaptor();
 MathJax._.handlers.html_ts.RegisterHTMLHandler(adaptor)
 const html = MathJax._.mathjax.mathjax.document('', {
   InputJax: new MathJax._.input.tex_ts.TeX({ packages: ['base', 'mathtools', 'ams', 'amscd', 'braket', 'cancel', 'cases', 'color', 'gensymb', 'mhchem', 'physics'] }),
-  OutputJax: new MathJax._.output.svg_ts.SVG(),
+  OutputJax: new MathJax._.output.svg_ts.SVG({fontCache: 'none'}),
 });
 
 if (typeof globalThis !== 'undefined') {
