8580d629fc5ffd0ee2b8ebb4219681565b8393d9
Mayank Mohapatra <125661248+Mayank77maruti@users.noreply.github.com>
2025-02-23 17:04:06 +0000

    try

--- d2layouts/d2cycle/layout.go
+++ d2layouts/d2cycle/layout.go
@@ -117,7 +117,7 @@ func createCircularArc(edge *d2graph.Edge) {
 			tangentX /= mag
 			tangentY /= mag
 		}
-        const MIN_SEGMENT_LEN = 3.9999999999
+        const MIN_SEGMENT_LEN = 4.0
 
 		dx := lastPoint.X - secondLastPoint.X
 		dy := lastPoint.Y - secondLastPoint.Y
