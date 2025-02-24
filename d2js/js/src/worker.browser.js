1417ded66a2ef19514497d4e1be683ad93e21d86
delfino <delfino@dolphinlabs.co.uk>
2025-02-24 00:26:34 +0000

    d2js: fix browser worker elk check

--- d2js/js/src/worker.browser.js
+++ d2js/js/src/worker.browser.js
@@ -38,11 +38,6 @@ export function setupMessageHandler(isNode, port, initWasm) {
             const layout = await elk.layout(elkGraph2);
             globalThis.elkResult = layout;
           }
-          const elkGraph = await d2.getELKGraph(JSON.stringify(data));
-          const elkGraph2 = JSON.parse(elkGraph).data;
-          const layout = await elk.layout(elkGraph2);
-          globalThis.elkResult = layout;
-
           const result = await d2.compile(JSON.stringify(data));
           const response = JSON.parse(result);
           if (response.error) throw new Error(response.error.message);
