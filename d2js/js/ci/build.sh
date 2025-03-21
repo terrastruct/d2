7887fb58424c6de7a4134a8dc3f7eab11f878943
Alexander Wang <alex@terrastruct.com>
2025-03-21 12:18:32 -0600

    update

--- d2js/js/ci/build.sh
+++ d2js/js/ci/build.sh
@@ -18,7 +18,7 @@ fi
 cd d2js/js
 sh_c bun build.js
 
-if [ -n "$NPM_VERSION" ]; then
+if [ -n "${NPM_VERSION:-}" ]; then
   cp package.json package.json.bak
   trap 'rm -f .npmrc; mv package.json.bak package.json' EXIT
 
