c7fc1af783ffcb61efb2308db5d33910d8872239
Alexander Wang <alex@terrastruct.com>
2025-05-01 20:18:33 -0600

    update

--- ci/release/release.sh
+++ ci/release/release.sh
@@ -3,24 +3,4 @@ set -eu
 cd -- "$(dirname "$0")/../.."
 . "./ci/sub/lib.sh"
 
-
-NPM_VERSION=""
-
-for arg in "$@"; do
-  case "$arg" in
-    --npm-version=*)
-      NPM_VERSION="${arg#*=}"
-      ;;
-  esac
-done
-
-if [ -z "$NPM_VERSION" ]; then
-  flag_errusage "--npm-version is required"
-fi
-
 ./ci/sub/release/release.sh "$@"
-
-if [ -n "$NPM_VERSION" ]; then
-  ./ci/release/release-js.sh --version="$NPM_VERSION"
-fi
-
